84711b2f0c97cbf6ee92b123ae4d6d2df3d7c540
GabsGullo <gabrielgullo30@gmail.com>
2023-12-03 21:18:34 -0300

    views file

---
+++ view/view/js/main.js
@@ -0,0 +1,24 @@
+async function login(){
+    let email = document.getElementById("emailInput").value;
+    let senha = document.getElementById("passwordInput").value;
+
+    console.log(email, senha);
+
+    const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: new Headers({
+            "Content-Type": "application/json; charset=utf8",
+
+            Accept: "application/json",
+        }),
+        body: JSON.stringify({
+            email: email,
+            senha: senha,
+        }),
+    });
+
+    let key = "Authorization";
+    let token = response.headers.get(key);
+    console.log(token);
+    window.localStorage.setItem(key, token);
+}
